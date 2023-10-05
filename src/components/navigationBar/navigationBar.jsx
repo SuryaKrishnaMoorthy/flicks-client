d7d81743c49d271faf519389a1090ae9b25503ff
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2023-10-04 23:02:43 -0400

    add filter of movies

--- src/components/navigationBar/navigationBar.jsx
+++ src/components/navigationBar/navigationBar.jsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-export const NavigationBar = ({ user, onLoggedOut }) => {
+export const NavigationBar = ({
+  user,
+  onLoggedOut,
+  filterMovies,
+  handleLoadAllMovies,
+}) => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    filterMovies(searchText);
+    setSearchText("");
+  };
+
   return (
     <Navbar bg="dark" data-bs-theme="dark" className="mb-4">
       <Container>
-        <Navbar.Brand as={Link} to="/">
+        <Navbar.Brand as={Link} to="/" onClick={handleLoadAllMovies}>
           Flicks
         </Navbar.Brand>
+        {user && (
+          <Form className="d-flex" onSubmit={handleSearch}>
+            <Form.Control
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+              type="search"
+              placeholder="Search movie"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button type="submit" variant="outline-dark">
+              Search
+            </Button>
+          </Form>
+        )}
         <Nav>
           {!user ? (
             <>
@@ -22,7 +53,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
             </>
           ) : (
             <>
-              <Nav.Link as={Link} to="/">
+              <Nav.Link as={Link} to="/" onClick={handleLoadAllMovies}>
                 Home
               </Nav.Link>
               <Nav.Link as={Link} to="/profile">
